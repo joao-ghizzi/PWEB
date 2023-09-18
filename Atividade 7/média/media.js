3a3fbbe1687bc85cf21e035d0c60bad5543243ec
joao-ghizzi <111469423+joao-ghizzi@users.noreply.github.com>
2023-09-18 12:30:12 -0300

    Add files via upload

---
+++ Atividade 7/média/media.js
@@ -0,0 +1,9 @@
+
+var nota1;
+var nota2;
+var nota3;
+alert("escreva as notas do aluno:");
+nota1 = prompt("digite a nota 1:");
+nota2 = prompt("digite a nota 2:");
+nota3 = prompt("digite a nota 3:");
+alert("a média é:" + (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3)) / 3);
---
+++ Atividade 7/operações/operacoes.js
@@ -0,0 +1,21 @@
+
+    var numero1;
+    var numero2;
+    var resultado;
+alert("escreva dois numeros para serem somados")
+numero1 = prompt("digite o numero 1:");
+numero2 = prompt("digite o numero 2:");
+resultado = parseFloat(numero2) + parseFloat(numero1);
+alert("a soma é: " + resultado);
+
+resultado =  parseFloat(numero2) - parseFloat(numero1);
+alert("a subtração é: " + resultado);
+
+resultado = parseFloat(numero2) * parseFloat(numero1);
+alert("o produto é: " + resultado);
+
+resultado = parseFloat(numero2) / parseFloat(numero1);
+alert("a divisão é: " + resultado);
+
+resultado = parseFloat(numero2) % parseFloat(numero1);
+alert("o resto é: " + resultado);
