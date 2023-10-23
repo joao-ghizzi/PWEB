6f5881818269691015d2f5457f66148d7f06e1c8
Joao <joaoghizzi9@gmail.com>
2023-10-23 16:57:15 -0300

    Atividade 14 completa

--- Atividade14/main.js
+++ Atividade14/main.js
@@ -1,8 +1,16 @@
-function result(params) {
+function result() {
     let texto = document.getElementById('texto').value;
     let maiusculas = document.getElementById('maiusculas').checked;
     if (maiusculas) {
         texto = texto.toUpperCase();
     }
+    document.getElementById('result').textContent = texto;
 
+    return false; // Isso impede que o formulário seja enviado, evitando o recarregamento da página
 }
+
+
+
+
+
+
