aa967e8b52839669d2ed771ffd59e99826f46cb2
joao-ghizzi <111469423+joao-ghizzi@users.noreply.github.com>
2023-11-13 12:36:01 -0300

    Add files via upload

---
+++ Atividade 18/app/routes/professores.js
@@ -0,0 +1,27 @@
+module.exports = function(app){
+    app.get('/informacao/professores', function(req,res){
+    const sql = require ('mssql/msnodesqlv8');
+    const sqlConfig = {
+    user: 'BD2211007',
+    password: 'A12345678a',
+    database: 'BD', //Na FATEC, utilizar o database BD ou LP8
+    server: 'apolo',//Caso o nome tenha uma instância, colocar duas
+    }
+   
+    async function getProfessores() {
+        try {
+            const pool = await sql.connect(sqlConfig);
+           
+            const results = await pool.request().query('SELECT * from PROFESSORES');
+            
+
+            res.render('informacao/professores',{profs : results.recordset}); 
+           
+            } catch (err) {
+            console.log(err);
+            }
+            //res.render('informacao/professores');
+            }
+            getProfessores();
+            });
+           }
